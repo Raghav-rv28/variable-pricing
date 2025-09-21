e414f063277fbafa9d8b2b51119df0563f5c2b20
Raghav-rv28 <raghavrudhra28@gmail.com>
2025-09-21 13:52:25 -0400

    pushing the change

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,21 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
 declare global {
   var prismaGlobal: InstanceType<typeof PrismaClient>;
 }
 
+const adapter = new PrismaLibSQL({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient({ adapter });
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma = global.prismaGlobal ?? new PrismaClient({ adapter });
 
 export default prisma;
