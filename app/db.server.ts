d998283ef47572cfae5b8b0ffec37526bc1b8e42
Raghav-rv28 <raghavrudhra28@gmail.com>
2025-09-21 17:16:24 -0400

    new db setup

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,21 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
 declare global {
   var prismaGlobal: InstanceType<typeof PrismaClient>;
 }
 
-const adapter = new PrismaLibSQL({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
-});
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient({ adapter });
+    global.prismaGlobal = new PrismaClient();
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient({ adapter });
+const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
